868f5ead05f3c1e63073625d207ec7051e6d7e75
shirok1 <tomatroom@qq.com>
2022-04-27 22:48:42 +0800

    fix: system env not read

--- Env.fs
+++ Env.fs
@@ -1,9 +1,23 @@
 module Env
 
+open System
+open System.Collections
+open System.Collections.Generic
 open dotenv.net
 
 
-let private env = lazy DotEnv.Read()
+let private env =
+    lazy
+        let dct = Dictionary<string, string>()
+
+        Environment.GetEnvironmentVariables()
+        |> Seq.cast<DictionaryEntry>
+        |> Seq.iter (fun kv -> dct.Add(string kv.Key, string kv.Value))
+
+        DotEnv.Read()
+        |> Seq.iter (fun (KeyValue (k, v)) -> dct.Add(k, v))
+
+        dct
 
 let GetEnv key =
     match env.Value.TryGetValue key with
